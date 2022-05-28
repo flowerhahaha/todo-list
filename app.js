cb1e4171ecb8e0c7d2440a8bc7d58350080a2487
Yen <p12427@gmail.com>
2022-05-28 18:23:37 +0800

    feat: add server and mongoose

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
-const port = 3000
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(process.env.MONGODB_URI)
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hi')
 })
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log('The server is running')
 })
