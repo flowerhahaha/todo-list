840f50dbd1435c15ed5de45d6af026c93005965a
Yen <p12427@gmail.com>
2022-05-30 14:51:20 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -77,17 +77,16 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// app.post('/todos/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   const name = req.body.name
-//   return Todo.findById(id)
-//     .then(todo => {
-//       todo.name = name
-//       return todo.save()
-//     })
-//     .then(()=> res.redirect(`/todos/${id}`))
-//     .catch(error => console.log(error))
-// })
+// set router: delete todo
+app.post('/todos/:id/delete', (req, res) => {
+  const {id} = req.params
+  Todo.findById(id)
+    .then(todo => {
+      todo.remove()
+      res.redirect('/')
+    })
+    .catch(error => console.log(error))
+})
 
 // start the server at port 3000
 app.listen(3000, () => {
