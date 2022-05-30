06e3e6e8c051f3acc8002f3179e372dd82ca201b
Yen <p12427@gmail.com>
2022-05-30 11:44:30 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const app = express()
 
 // set template engine
 const exphbs = require('express-handlebars')
-const { TopologyDescription } = require('mongodb')
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
 // set database
+const { TopologyDescription } = require('mongodb')
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const Todo = require('./models/todo')
-
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -24,6 +23,10 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// set body-parser 
+app.use(express.urlencoded({ extended: true }))
+
+// set router: get homepage
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
@@ -31,7 +34,20 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-// set router
+// set router: get new page
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+// set router: post a new todo
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  Todo.create({'name': req.body.name.toString()})
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// start the server at port 3000
 app.listen(3000, () => {
   console.log('The server is running')
 })
