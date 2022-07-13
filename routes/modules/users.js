1f4b4796b7be211cb4221ed643811d1a7ed4355c
Yen <p12427@gmail.com>
2022-07-13 14:44:38 +0800

    feat: add login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,14 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// post login information to login personal todo list page
+router.post('/login', (req, res) => {
+
+})
+
+// get register page
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
